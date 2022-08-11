a9c4bd5489b8fa2b6790dd165216448f9c6d6436
Anshu Kumar <anshu8581@gmail.com>
2022-08-11 12:29:28 +0530

    Amended SAF Apply

--- app/Repository/SAF/EloquentSafRepository.php
+++ app/Repository/SAF/EloquentSafRepository.php
@@ -106,41 +106,45 @@ class EloquentSafRepository implements SafRepository
             $saf->save();
 
             // SAF Owner Details
-            $owner_detail = $request['owner'];
-            foreach ($owner_detail as $owner_details) {
-                $owner = new ActiveSafOwnerDetail;
-                $owner->saf_dtl_id = $saf->id;
-                $owner->owner_name = $owner_details['ownerName'];
-                $owner->guardian_name = $owner_details['guardianName'];
-                $owner->relation_type = $owner_details['relation'];
-                $owner->mobile_no = $owner_details['mobileNo'];
-                $owner->email = $owner_details['email'];
-                $owner->pan_no = $owner_details['pan'];
-                $owner->aadhar_no = $owner_details['aadhar'];
-                $owner->emp_details_id = $owner_details['empDetail'];
-                $owner->rmc_saf_owner_dtl_id = $owner_details['rmcSafOwnerDtl'];
-                $owner->rmc_saf_dtl_id = $owner_details['rmcSafDetail'];
-                $owner->gender = $owner_details['gender'];
-                $owner->dob = $owner_details['dob'];
-                $owner->is_armed_force = $owner_details['isArmedForce'];
-                $owner->is_specially_abled = $owner_details['isSpeciallyAbled'];
-                $owner->save();
+            if ($request['owner']) {
+                $owner_detail = $request['owner'];
+                foreach ($owner_detail as $owner_details) {
+                    $owner = new ActiveSafOwnerDetail;
+                    $owner->saf_dtl_id = $saf->id;
+                    $owner->owner_name = $owner_details['ownerName'];
+                    $owner->guardian_name = $owner_details['guardianName'];
+                    $owner->relation_type = $owner_details['relation'];
+                    $owner->mobile_no = $owner_details['mobileNo'];
+                    $owner->email = $owner_details['email'];
+                    $owner->pan_no = $owner_details['pan'];
+                    $owner->aadhar_no = $owner_details['aadhar'];
+                    $owner->emp_details_id = $owner_details['empDetail'];
+                    $owner->rmc_saf_owner_dtl_id = $owner_details['rmcSafOwnerDtl'];
+                    $owner->rmc_saf_dtl_id = $owner_details['rmcSafDetail'];
+                    $owner->gender = $owner_details['gender'];
+                    $owner->dob = $owner_details['dob'];
+                    $owner->is_armed_force = $owner_details['isArmedForce'];
+                    $owner->is_specially_abled = $owner_details['isSpeciallyAbled'];
+                    $owner->save();
+                }
             }
 
             // Floor Details
-            $floor_detail = $request['floor'];
-            foreach ($floor_detail as $floor_details) {
-                $floor = new ActiveSafFloorDetail();
-                $floor->saf_dtl_id = $saf->id;
-                $floor->floor_mstr_id = $floor_details['floorNo'];
-                $floor->usage_type_mstr_id = $floor_details['useType'];
-                $floor->const_type_mstr_id = $floor_details['constructionType'];
-                $floor->occupancy_type_mstr_id = $floor_details['occupancyType'];
-                $floor->builtup_area = $floor_details['buildupArea'];
-                $floor->date_from = $floor_details['dateFrom'];
-                $floor->date_upto = $floor_details['dateUpto'];
-                $floor->prop_floor_details_id = $floor_details['propFloorDetail'];
-                $floor->save();
+            if ($request['floor']) {
+                $floor_detail = $request['floor'];
+                foreach ($floor_detail as $floor_details) {
+                    $floor = new ActiveSafFloorDetail();
+                    $floor->saf_dtl_id = $saf->id;
+                    $floor->floor_mstr_id = $floor_details['floorNo'];
+                    $floor->usage_type_mstr_id = $floor_details['useType'];
+                    $floor->const_type_mstr_id = $floor_details['constructionType'];
+                    $floor->occupancy_type_mstr_id = $floor_details['occupancyType'];
+                    $floor->builtup_area = $floor_details['buildupArea'];
+                    $floor->date_from = $floor_details['dateFrom'];
+                    $floor->date_upto = $floor_details['dateUpto'];
+                    $floor->prop_floor_details_id = $floor_details['propFloorDetail'];
+                    $floor->save();
+                }
             }
 
             DB::commit();
