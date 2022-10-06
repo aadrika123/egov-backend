8f6fb049897ce868a698f574857d077c50bf0e3f
frameworkfuturistic <frameworkfuturistic@gmail.com>
2022-10-06 18:31:37 +0530

    Water Module

---
+++ routes/water.php
@@ -0,0 +1,13 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * | Created On-06-10-2022 
+ * | Created For-The Routes defined for the Water Usage Charge Management System Module
+ * | Created By-Anshu Kumar
+ */
+
+Route::get('/', function () {
+    dd('Welcome to simple Water route file.');
+});
