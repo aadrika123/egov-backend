3c11cec7d4b3ce4355e2e3b844ca5c3e240c6385
SandeepBara <sandeepbara830@gmail.com>
2022-10-07 11:45:20 +0530

    update models

---
+++ app/Models/Trade/TradeParamCategoryType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Trade\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TradeParamCategoryType extends Model
+{
+    use HasFactory;
+}
