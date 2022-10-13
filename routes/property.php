896af1fea840b4be8995b703795f61a97f1257d1
anshukumar11062 <anshu8581@gmail.com>
2022-10-13 17:35:37 +0530

    SAF WORKFLOW

--- routes/property.php
+++ routes/property.php
@@ -30,7 +30,8 @@ Route::group(['middleware' => ['json.response', 'auth:sanctum', 'request_logger'
     // SAF 
     Route::controller(ActiveSafController::class)->group(function () {
         Route::match(["get", "post"], 'saf/apply/{assessmentType}', 'applySaf');                            // Applying Saf Route
-        Route::get('saf/inbox/{key?}', 'inbox');                             // Saf workflow Inbox and Inbox By search key
+        //Route::get('saf/inbox/{key?}', 'inbox');                             // Saf workflow Inbox and Inbox By search key
+        Route::get('saf/inbox', 'inbox');                                      // Saf Inbox
         Route::get('saf/outbox/{key?}', 'outbox');                           // Saf Workflow Outbox and Outbox By search key
         Route::get('saf/details/{id}', 'details');                           // Saf Workflow safDetails and safDetails By ID
         Route::post('saf/escalate/{id?}', 'special');                         // Saf Workflow special and safDetails By id
