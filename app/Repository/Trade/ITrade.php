c780fa428046b8daa5ff77b2329439f3de47e1e2
SandeepBara <sandeepbara830@gmail.com>
2022-10-18 11:47:50 +0530

    update api inbox

--- app/Repository/Trade/ITrade.php
+++ app/Repository/Trade/ITrade.php
@@ -18,7 +18,7 @@ interface ITrade
     public function applyApplication(Request $request);
     public function validate_holding_no(Request $request);
     public function searchLicenceByNo(Request $request);
-    public function searchLicence(string $licence_no);
+    public function searchLicence(string $licence_no,$ulb_id);
     public function updateBasicDtl(Request $request);
     public function getLicenceDtl($id);
     public function paybleAmount(Request $request);
@@ -28,5 +28,6 @@ interface ITrade
     public function getownershipTypeList();
     public function gettradeitemsList();
     public function getAllApplicationType();
-    public function inbox($key);
+    public function inbox(Request $request);
+    public function outbox(Request $request);
 }
