b10f8c363cfdfede870854e7aae7d6c05eca17f0
anshukumar11062 <anshu8581@gmail.com>
2022-11-14 19:04:53 +0530

    Insert SAF Tax

---
+++ app/EloquentClass/Property/InsertTax.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EloquentClass\Property;
+
+use App\Models\Property\SafsDemand;
+
+class InsertTax
+{
+    public function insertTax($safId, $userId, $demand)
+    {
+        $details = $demand['original']['data']['details'];
+        $filtered = $details->toArray();
+        $a = collect($filtered)->only(['totalTax']);
+        return $a;
+
+        foreach ($details as $detail) {
+            $safDemand = new SafsDemand();
+            $safDemand->saf_id = $safId;
+            $safDemand->qtr = $detail['qtr'];
+            $safDemand->holding_tax = $detail['holdingTax'];
+            $safDemand->water_tax = $detail['waterTax'];
+            $safDemand->education_cess = $detail['educationTax'];
+            $safDemand->health_cess = $detail['healthTax'];
+            $safDemand->latrine_tax = $detail['latrineTax'];
+            $safDemand->additional_tax = 0;
+            $safDemand->amount = $detail['totalTax'];
+            $safDemand->balance = $detail['totalTax'];
+            $safDemand->paid_status = 0;
+            $safDemand->arv = $detail['arv'];
+            $safDemand->rwh_penalty = $detail['rwhPenalty'];
+            $safDemand->fyear = $detail['quarterYear'];
+            $safDemand->due_date = $detail['dueDate'];
+            $safDemand->status = 1;
+            $safDemand->user_id = $userId;
+            $safDemand->save();
+        }
+    }
+}
