68b9997c520df09d19f4b86a9d4b0e66147edfac
mrinal9911 <mrinalk123456@gmail.com>
2022-11-26 11:17:39 +0530

    dfh

--- routes/property.php
+++ routes/property.php
@@ -127,9 +127,9 @@ Route::group(['middleware' => ['json.response', 'auth:sanctum', 'request_logger'
 
         Route::get('objection/inbox', 'inbox');
         Route::get('objection/outbox', 'outbox');
-        Route::post('concession/next-level', 'postNextLevel');
-        Route::post('concession/approvalrejection', 'approvalRejection');
-        Route::post('concession/backtocitizen', 'backToCitizen');
+        Route::post('objection/next-level', 'postNextLevel');
+        Route::post('objection/approvalrejection', 'approvalRejection');
+        Route::post('objection/backtocitizen', 'backToCitizen');
     });
 
     Route::controller(CalculatorController::class)->group(function () {
