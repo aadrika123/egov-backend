d5a67df70829dafa5d3b4f50908f975c4f63cee2
SandeepBara <sandeepbara830@gmail.com>
2022-12-05 12:00:34 +0530

    update document api

--- app/Repository/Trade/Trade.php
+++ app/Repository/Trade/Trade.php
@@ -1339,27 +1339,27 @@ class Trade implements ITrade
             $mDocumentsList = $this->getDocumentTypeList($refLicence);                 
             foreach($mDocumentsList as $val)
             {   
-                $data["documentsList"][$val->doc_for] = $this->getDocumentList($val->doc_for,$refLicence->application_type_id,$val->show);
-                $data["documentsList"][$val->doc_for]["is_mandatory"] = $val->is_mandatory;
+                $data["documentsList"]["docList"][$val->doc_for] = $this->getDocumentList($val->doc_for,$refLicence->application_type_id,$val->show);
+                $data["documentsList"]["isMedetory"][$val->doc_for] = $val->is_mandatory;
             }
             if($refLicence->application_type_id==1)
             {                
-                $data["documentsList"]["Identity Proof"] = $this->getDocumentList("Identity Proof",$refLicence->application_type_id,0);
-                $data["documentsList"]["Identity Proof"]["is_mandatory"] = 1;
+                $data["documentsList"]["docList"]["Identity Proof"] = $this->getDocumentList("Identity Proof",$refLicence->application_type_id,0);
+                $data["documentsList"]["isMedetory"]["Identity Proof"] = 1;
             }
             $doc = (array) null;
-            foreach($data["documentsList"] as $key => $val)
+            foreach($data["documentsList"]["docList"] as $key => $val)
             {
                 if($key == "Identity Proof")
                 {
                     continue;
                 }
-                $data["documentsList"][$key]["doc"] = $this->check_doc_exist($licenceId,$key);
-                if(isset($data["documentsList"][$key]["doc"]["document_path"]))
+                $data["documentsList"]['uploadDocs'][$key] = $this->check_doc_exist($licenceId,$key);
+                if(isset($data["documentsList"]['uploadDocs'][$key]["document_path"]))
                 {
-                    $path = $this->readDocumentPath($data["documentsList"][$key]["doc"]["document_path"]);
+                    $path = $this->readDocumentPath($data["documentsList"]['uploadDocs'][$key]["document_path"]);
                     // $data["documentsList"][$key]["doc"]["document_path"] = !empty(trim($data["documentsList"][$key]["doc"]["document_path"]))?storage_path('app/public/' . $data["documentsList"][$key]["doc"]["document_path"]):null;
-                    $data["documentsList"][$key]["doc"]["document_path"] = !empty(trim($data["documentsList"][$key]["doc"]["document_path"]))?$path :null;
+                    $data["documentsList"]['uploadDocs'][$key]["document_path"] = !empty(trim($data["documentsList"]['uploadDocs'][$key]["document_path"]))?$path :null;
 
                 }
             } 
@@ -1391,7 +1391,7 @@ class Trade implements ITrade
             $data["owneres"] = $refOwneres;
             $data["uploadDocument"] = $mUploadDocument;
             if($request->getMethod()=="GET")
-            {
+            { 
                 return responseMsg(true,"",$data);
 
             }
@@ -2529,7 +2529,6 @@ class Trade implements ITrade
             $role_id = $role->role_id;
             $apply_from = $this->_parent->userType($refWorkflowId);            
             $rules = [
-                // "receiverId" => "required|int",
                 "btn" => "required|in:btc,forward,backward",
                 "licenceId" => "required|digits_between:1,9223372036854775807",
                 "comment" => "required|min:10|regex:$regex",
@@ -3407,7 +3406,86 @@ class Trade implements ITrade
             return responseMsg(false, $e->getMessage(), $request->all());
         }
 
-    }   
+    } 
+    
+    public function approvedApplication(Request $request)
+    {
+        try
+        {
+            $refUser        = Auth()->user();
+            $refUserId      = $refUser->id;
+            $refUlbId       = $refUser->ulb_id;
+            $mWardPermission = $this->_modelWard->getAllWard($refUlbId)->map(function($val){
+                $val->ward_no = $val->ward_name;
+                return $val;
+            });
+            $mWardPermission = objToArray($mWardPermission);
+            $licence = ActiveLicence::select("active_licences.id",
+                                            "active_licences.application_no",
+                                            "active_licences.provisional_license_no",
+                                            "active_licences.license_no",
+                                            "active_licences.document_upload_status",
+                                            "active_licences.payment_status",
+                                            "active_licences.firm_name",
+                                            "active_licences.apply_date",
+                                            "active_licences.apply_from",
+                                            "owner.owner_name",
+                                            "owner.guardian_name",
+                                            "owner.mobile_no",
+                                            "owner.email_id",
+                                            )
+                            ->join(DB::raw("(select STRING_AGG(owner_name,',') AS owner_name,
+                                STRING_AGG(guardian_name,',') AS guardian_name,
+                                STRING_AGG(mobile::TEXT,',') AS mobile_no,
+                                STRING_AGG(emailid,',') AS email_id,
+                                licence_id
+                                FROM active_licence_owners 
+                                WHERE status =1
+                                GROUP BY licence_id
+                                )owner"),function($join){
+                                $join->on("owner.licence_id","active_licences.id");
+                            })
+                            ->where("active_licences.status",1)    
+                            ->where("active_licences.pending_status",5)                     
+                            ->where("active_licences.ulb_id",$refUlbId);
+                            if(isset($inputs['key']) && trim($inputs['key']))
+                            {
+                                $key = trim($inputs['key']);
+                                $licence = $licence->where(function ($query) use ($key) {
+                                $query->orwhere('active_licences.holding_no', 'ILIKE', '%' . $key . '%')
+                                ->orwhere('active_licences.application_no', 'ILIKE', '%' . $key . '%')
+                                ->orwhere("active_licences.license_no", 'ILIKE', '%' . $key . '%')
+                                ->orwhere("active_licences.provisional_license_no", 'ILIKE', '%' . $key . '%')                                            
+                                ->orwhere('owner.owner_name', 'ILIKE', '%' . $key . '%')
+                                ->orwhere('owner.guardian_name', 'ILIKE', '%' . $key . '%')
+                                ->orwhere('owner.mobile_no', 'ILIKE', '%' . $key . '%');
+                                });
+                            }
+                            if(isset($inputs['wardNo']) && trim($inputs['wardNo']) && $inputs['wardNo']!="ALL")
+                            {
+                                $mWardIds =$inputs['wardNo']; 
+                                $licence = $licence
+                                            ->whereIn('active_licences.ward_mstr_id', $mWardIds);
+                            }
+                            if(isset($inputs['formDate']) && isset($inputs['toDate']) && trim($inputs['formDate']) && $inputs['toDate'])
+                            {
+                                $licence = $licence
+                                    ->whereBetween('licence_level_pendings.created_at::date',[$inputs['formDate'],$inputs['formDate']]); 
+                            }
+            $licence = $licence
+                    ->get();
+            $data = [
+                "wardList"=>$mWardPermission,                
+                "licence"=>$licence,
+            ] ;           
+            return responseMsg(true, "", $data);
+
+        }
+        catch(Exception $e)
+        {
+            return responseMsg(false, $e->getMessage(), $request->all());
+        }
+    }
 
     #------------------- Reports function ------------------
 
