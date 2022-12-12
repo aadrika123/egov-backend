ac8181eaf460f508509ee46ede2f4b49090bc275
SandeepBara <sandeepbara830@gmail.com>
2022-12-12 11:09:53 +0530

    upda property bi

--- app/Repository/Property/Concrete/PropertyBifurcation.php
+++ app/Repository/Property/Concrete/PropertyBifurcation.php
@@ -79,7 +79,7 @@ class PropertyBifurcation implements IPropertyBifurcation
                 ->orderBy("id", "desc")
                 ->first();
             if ($priv_data) {
-                throw new Exception("Assesment already apply");
+                throw new Exception("Assesment already appled");
             }
             $mOwrners  = $this->_property->getPropOwnerByProId($mProperty->id);
             $mFloors    = $this->getFlooreDtl($mProperty->id);
