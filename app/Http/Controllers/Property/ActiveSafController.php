13d6cecf882f2b250a64464349c2381006b5336b
frameworkfuturistic <anshu8581@gmail.com>
2023-01-10 17:52:31 +0530

    Get Full Data in Workflow

--- app/Http/Controllers/Property/ActiveSafController.php
+++ app/Http/Controllers/Property/ActiveSafController.php
@@ -652,35 +652,34 @@ class ActiveSafController extends Controller
                 'data' => $cardDetails
             ];
             $fullDetailsData['fullDetailsData']['cardArray'] = $cardElement;
-            return responseMsgs(true, 'Data Fetched', remove_null($fullDetailsData), "010104", "1.0", "303ms", "POST", $req->deviceId);
             $data = json_decode(json_encode($data), true);
             $metaReqs['customFor'] = 'SAF';
-            $metaReqs['wfRoleId'] = $data['Basic Details']['current_role'];
-            $metaReqs['workflowId'] = $data['Basic Details']['workflow_id'];
-            $metaReqs['lastRoleId'] = $data['Basic Details']['last_role_id'];
+            $metaReqs['wfRoleId'] = $data['current_role'];
+            $metaReqs['workflowId'] = $data['workflow_id'];
+            $metaReqs['lastRoleId'] = $data['last_role_id'];
 
-            $citizenComment = $mWorkflowTracks->getTracksByRefId($mRefTable, $data['Basic Details']['id']);
-            $data['citizenComment'] = $citizenComment;
+            $citizenComment = $mWorkflowTracks->getTracksByRefId($mRefTable, $data['id']);
+            $fullDetailsData['citizenComment'] = $citizenComment;
 
             $req->request->add($metaReqs);
             $forwardBackward = $forwardBackward->getRoleDetails($req);
-            $data['roleDetails'] = collect($forwardBackward)['original']['data'];
+            $fullDetailsData['roleDetails'] = collect($forwardBackward)['original']['data'];
 
-            $data['timelineData'] = collect($req);
+            $fullDetailsData['timelineData'] = collect($req);
 
             $custom = $mCustomDetails->getCustomDetails($req);
-            $data['departmentalPost'] = collect($custom)['original']['data'];
+            $fullDetailsData['departmentalPost'] = collect($custom)['original']['data'];
 
             $docList = $getDocuments->getUploadDocuments($req);
-            $data['documentList'] = collect($docList)['original']['data'];
+            $fullDetailsData['documentList'] = collect($docList)['original']['data'];
 
-            $a = $getDocuments->getDocList($req);
-            $b = collect($a)['original']['data']['documentsList'];
-            $data['docrequired'] = collect($b)->map(function ($value) {
+            $getDocumentList = $getDocuments->getDocList($req);
+            $documentList = collect($getDocumentList)['original']['data']['documentsList'];
+            $fullDetailsData['docrequired'] = collect($documentList)->map(function ($value) {
                 return $value['docVal'];
             });
 
-            return responseMsgs(true, 'Data Fetched', remove_null($data), "010104", "1.0", "303ms", "POST", $req->deviceId);
+            return responseMsgs(true, 'Data Fetched', remove_null($fullDetailsData), "010104", "1.0", "303ms", "POST", $req->deviceId);
         } catch (Exception $e) {
             return responseMsg(false, $e->getMessage(), "");
         }
