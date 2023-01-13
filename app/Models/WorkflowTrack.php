542e833fefb5f913428533f305cf4b91d2b0b2f4
frameworkfuturistic <anshu8581@gmail.com>
2023-01-13 11:02:35 +0530

    Workflow Tracks

--- app/Models/WorkflowTrack.php
+++ app/Models/WorkflowTrack.php
@@ -67,12 +67,15 @@ class WorkflowTrack extends Model
                 'workflow_tracks.track_date',
                 'workflow_tracks.forward_date',
                 'workflow_tracks.forward_time',
-                'w.role_name as commentedBy'
+                'w.role_name as commentedBy',
+                'wr.role_name as forwarded_to'
             )
             ->where('ref_table_dot_id', $mRefTable)
             ->where('ref_table_id_value', $tableId)
             ->join('wf_roles as w', 'w.id', '=', 'workflow_tracks.sender_role_id')
+            ->join('wf_roles as wr', 'wr.id', '=', 'workflow_tracks.receiver_role_id')
             ->where('citizen_id', null)
+            ->orderByDesc('workflow_tracks.id')
             ->get();
     }
 
