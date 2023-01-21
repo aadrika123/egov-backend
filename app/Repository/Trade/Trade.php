bb740692f8a2503a9741b2592011795c2d0eba12
mrinal9911 <mrinalk123456@gmail.com>
2023-01-21 11:28:38 +0530

    hgfh

--- app/Repository/Trade/Trade.php
+++ app/Repository/Trade/Trade.php
@@ -1259,16 +1259,22 @@ class Trade implements ITrade
                     $doc2 = (array) null;
                     $doc2["ownerId"] = $val->id;
                     $doc2["ownerName"] = $val->owner_name;
-                    $doc2["docName"]   = "image";
+                    $doc2["docName"]   = "Photo";
                     $doc2['isMadatory'] = 0;
-                    $doc2['docVal'][] = ["id" => 0, "doc_name" => "Photo"];
-                    $refOwneres[$key]["image"] = $this->check_doc_exist_owner($licenceId, $val->id, 0);
-                    $doc2['uploadDoc'] = $refOwneres[$key]["image"];
-                    if (isset($refOwneres[$key]["image"]["document_path"])) {
-                        $path = $this->readDocumentPath($refOwneres[$key]["image"]["document_path"]);
-                        $refOwneres[$key]["image"]["document_path"] = !empty(trim($refOwneres[$key]["image"]["document_path"])) ? $path : null;
-                        $doc2['uploadDoc']["document_path"] = $path;
-                    }
+                    $doc2['docVal'][] = ["id" => 93, "doc_name" => "Photo"];
+
+                    $refdocumentId = collect($doc2['docVal'])->map(function ($value, $key) {
+                        return $value['id'];
+                    });
+                    $doc2['uploadDoc'] = $mWfActiveDocument->getTradeAppByAppNoDocId($refLicence->application_no, $refdocumentId);
+                    // $doc2['docVal'][] = ["id" => 93, "doc_name" => "Photo"];
+                    // $refOwneres[$key]["image"] = $this->check_doc_exist_owner($licenceId, $val->id, 0);
+                    // $doc2['uploadDoc'] = $refOwneres[$key]["image"];
+                    // if (isset($refOwneres[$key]["image"]["document_path"])) {
+                    //     $path = $this->readDocumentPath($refOwneres[$key]["image"]["document_path"]);
+                    //     $refOwneres[$key]["image"]["document_path"] = !empty(trim($refOwneres[$key]["image"]["document_path"])) ? $path : null;
+                    //     $doc2['uploadDoc']["document_path"] = $path;
+                    // }
                     array_push($ownersDoc, $doc2);
                     array_push($testOwnersDoc[$key], $doc2);
                 }
