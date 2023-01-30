8c03838ec3b3d76ffd72a6f759952730ca8d2f1e
frameworkfuturistic <anshu8581@gmail.com>
2023-01-30 18:20:27 +0530

    Property Get Demand

--- app/Models/Property/PropDemand.php
+++ app/Models/Property/PropDemand.php
@@ -4,6 +4,7 @@ namespace App\Models\Property;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PropDemand extends Model
 {
@@ -18,5 +19,33 @@ class PropDemand extends Model
             ->orderByDesc('id')
             ->first();
         return $propDemand;
+    }
+
+    /**
+     * | Get Property Dues Demand by Property Id
+     */
+    public function getDueDemandByPropId($propId)
+    {
+        return PropDemand::select(
+            'id',
+            DB::raw("concat(qtr,'/',fyear) as quarterYear"),
+            'arv',
+            'qtr',
+            'holding_tax',
+            'water_tax',
+            'education_cess',
+            'health_cess',
+            'latrine_tax',
+            'additional_tax',
+            'amount',
+            'balance',
+            'fyear',
+            'adjust_amt'
+        )
+            ->where('property_id', $propId)
+            ->where('paid_status', 0)
+            ->where('status', 1)
+            ->orderByDesc('id')
+            ->get();
     }
 }
