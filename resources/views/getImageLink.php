6a310682a0d4398c3d8f7a32e46b25be663a2b0e
SandeepBara <sandeepbara830@gmail.com>
2023-02-03 16:08:01 +0530

    dfsdfsfds

--- resources/views/getImageLink.php
+++ resources/views/getImageLink.php
@@ -5,24 +5,33 @@ class getImageLink
     public static function getImage($path)
     {
         // $full_path = $_SERVER['DOCUMENT_ROOT'].'/RMCDMC/writable/uploads/'.$path;
-        $full_path = storage_path("app/public/$path");
-        // dd( $full_path);
-        if(!file_exists($full_path))
+        $storage_path = storage_path("app/public/$path");
+        $public_path = public_path("$path");
+        $image = false;
+        if(file_exists($storage_path) && !$image)
         {
-            die("File not available.");
+            $image = $storage_path;
+        }
+        if(file_exists($public_path) && !$image)
+        {
+            $image = $public_path;
+        }
+        if(!$image)
+        {
+            die("File Not Found!.....");
         }
 
-        $getInfo = getimagesize($full_path);
-        $explod_path = explode('.', $full_path);
+        $getInfo = getimagesize($image);
+        $explod_path = explode('.', $image);
         $exp = end($explod_path);
         
-        if(file_exists($full_path) && !isset($getInfo['mime']))
+        if(file_exists($image) && !isset($getInfo['mime']))
         $getInfo['mime']='application/pdf';
         
         if($getInfo['mime']=='application/pdf')
         {
             header('Content-type: '. $getInfo['mime']);
-            header('Content-Length: ' . filesize($full_path));
+            header('Content-Length: ' . filesize($image));
             header('Cache-Control: no-cache');
             header('Content-Transfer-Encoding: binary'); 
             header('Accept-Ranges: bytes');
@@ -30,15 +39,15 @@ class getImageLink
         else
         {
             header('Content-type: '. $getInfo['mime']);
-            header('Content-Length: ' . filesize($full_path));
+            header('Content-Length: ' . filesize($image));
             header('Cache-Control: no-cache');
         }
         header('Content-type: ' . $getInfo['mime']);
-        header('Content-Length: ' . filesize($full_path));
+        header('Content-Length: ' . filesize($image));
         header('Cache-Control: no-cache');
         ob_clean();
         flush();
-        return readfile($full_path);
+        return readfile($image);
     }
 }
 
