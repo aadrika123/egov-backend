4e3df0b12f351e4f4188b7f29b1deb147a043810
mrinal9911 <mrinalk123456@gmail.com>
2023-02-10 18:30:29 +0530

    commit

--- app/Models/Property/PropActiveSafsFloor.php
+++ app/Models/Property/PropActiveSafsFloor.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class PropActiveSafsFloor extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'saf_id', 'floor_mstr_id', 'usage_type_mstr_id', 'const_type_mstr_id', 'occupancy_type_mstr_id',
+        'builtup_area', 'date_from', 'date_upto', 'user_id', 'prop_floor_details_id', 'carpet_area'
+    ];
 
     /**
      * | Get Saf Floor Details by SAF id
@@ -59,5 +64,56 @@ class PropActiveSafsFloor extends Model
             ->where('saf_id', $safId)
             ->orderByDesc('ref_prop_usage_types.id')
             ->get();
+    }
+
+    /**
+     * | Floor Edit
+     */
+    public function editFloor($req, $citizenId)
+    {
+        $req = new Request($req);
+        $floor = PropActiveSafsFloor::find($req->floorId);
+        if ($req->occupancyType == 1) {
+            $carpetArea =  $req->buildupArea * 0.70;
+        } else {
+            $carpetArea =  $req->buildupArea * 0.80;
+        }
+        $reqs = [
+            'floor_mstr_id' => $req->floorNo,
+            'usage_type_mstr_id' => $req->useType,
+            'const_type_mstr_id' => $req->constructionType,
+            'occupancy_type_mstr_id' => $req->occupancyType,
+            'builtup_area' => $req->buildupArea,
+            'carpet_area' => $carpetArea,
+            'date_from' => $req->dateFrom,
+            'date_upto' => $req->dateUpto,
+            // 'prop_floor_details_id' => $req->propFloorDetail,
+            'user_id' => $citizenId,
+
+        ];
+        $floor->update($reqs);
+    }
+
+    public function addfloor($req, $safId, $citizenId)
+    {
+        if ($req['occupancyType'] == 1) {
+            $carpetArea =  $req['buildupArea'] * 0.70;
+        } else {
+            $carpetArea =  $req['buildupArea'] * 0.80;
+        }
+
+        $floor = new  PropActiveSafsFloor();
+        $floor->saf_id = $safId;
+        $floor->floor_mstr_id = $req['floorNo'] ?? null;
+        $floor->usage_type_mstr_id = $req['useType'] ?? null;
+        $floor->const_type_mstr_id = $req['constructionType'] ?? null;
+        $floor->occupancy_type_mstr_id = $req['occupancyType'] ??  null;
+        $floor->builtup_area = $req['buildupArea'] ?? null;
+        $floor->carpet_area = $carpetArea;
+        $floor->date_from = $req['dateFrom'] ?? null;
+        $floor->date_upto = $req['dateUpto'] ?? null;
+        $floor->prop_floor_details_id = $req['propFloorDetail'] ?? null;
+        $floor->user_id = $citizenId;
+        $floor->save();
     }
 }
