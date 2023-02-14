112b338176672ff0eb1586b4e30f509e6713febf
frameworkfuturistic <anshu8581@gmail.com>
2023-02-14 11:48:05 +0530

    memo receipt

--- app/Models/Property/PropSafMemoDtl.php
+++ app/Models/Property/PropSafMemoDtl.php
@@ -65,15 +65,24 @@ class PropSafMemoDtl extends Model
     public function getMemoDtlsByMemoId($memoId)
     {
         $query = "SELECT d.*,
-                        pd.holding_tax,
-                        pd.water_tax,
-                        pd.education_cess,
-                        pd.health_cess,
-                        pd.additional_tax
-                    
-                    FROM prop_saf_memo_dtls d
-                    JOIN prop_safs_demands pd ON pd.fyear=d.from_fyear AND pd.qtr=d.from_qtr AND pd.saf_id=d.saf_id AND pd.status=1
-                    WHERE d.id=$memoId";
+                    TO_CHAR(d.created_at,'DD-MM-YYYY') AS memo_date,
+                                pd.holding_tax,
+                                pd.water_tax,
+                                pd.education_cess,
+                                pd.health_cess,
+                                pd.additional_tax,
+                                o.owner_name,
+                                o.guardian_name,
+                                s.prop_address,
+                                u.ward_name AS ward_no
+                            
+                            FROM prop_saf_memo_dtls d
+                            JOIN prop_safs_demands pd ON pd.fyear=d.from_fyear AND pd.qtr=d.from_qtr AND pd.saf_id=d.saf_id AND pd.status=1
+                            JOIN (SELECT owner_name,guardian_name,saf_id FROM prop_owners ORDER BY id) AS o ON o.saf_id=d.saf_id
+                            JOIN prop_active_safs AS s ON s.id=d.saf_id
+                            JOIN ulb_ward_masters AS u ON u.id=d.ward_mstr_id
+                            WHERE d.id=$memoId
+                    LIMIT 1";
         return DB::select($query);
     }
 }
