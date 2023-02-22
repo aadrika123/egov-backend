b2438079043e7a1acb4cd3abba2574ed28359f4d
SandeepBara <sandeepbara830@gmail.com>
2023-02-22 19:15:47 +0530

    fsfdsfsdf s

--- app/Models/Trade/TradeChequeDtl.php
+++ app/Models/Trade/TradeChequeDtl.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class TradeChequeDtl extends Model
 {
     use HasFactory;
-
+    public $timestamps=false;
 
     public function chequeDtlById($request)
     {
