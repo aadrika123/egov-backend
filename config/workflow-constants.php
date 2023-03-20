d670cc66d60d79e1f3f81053ffd192b030a1d3ea
mrinal9911 <mrinalk123456@gmail.com>
2023-03-20 11:12:07 +0530

    git ommmit

--- config/workflow-constants.php
+++ config/workflow-constants.php
@@ -11,25 +11,32 @@ return [
     "SAF_WORKFLOW_ID"           => "4",
     "SAF_REASSESSMENT_ID"       => "5",
     "SAF_MUTATION_ID"           => "9",
+    "GBSAF_NEW_ASSESSMENT"      => "10",
+    "GBSAF_REASSESSMENT"        => "11",
+    "SAF_BIFURCATION_ID"        => "25",
+    "SAF_AMALGAMATION_ID"       => "373",
 
-    "PROPERTY_CONCESSION_ID"    => "35",
-    "TRADE_WORKFLOW_ID"         => "10",
+    "PROPERTY_CONCESSION_ID"            => "35",
+    "TRADE_WORKFLOW_ID"                 => "10",
     "PROPERTY_DEACTIVATION_WORKFLOW_ID" => "167",
+    "PROPERTY_DEACTIVATION_MASTER_ID"   => "24",
     "PROPERTY_OBJECTION_CLERICAL"       => "36",
     "PROPERTY_OBJECTION_ASSESSMENT"     => "56",
     "PROPERTY_OBJECTION_FORGERY"        => "76",
-    "RAIN_WATER_HARVESTING_ID"  => "98",
-    "SAF_BIFURCATION_ID"        => "25",
-    "SAF_AMALGAMATION_ID"        => "373",
+    "RAIN_WATER_HARVESTING_ID"          => "98",
+
     // workflow master
-    "WATER_MASTER_ID"   => "3",
-    "TRADE_NOTICE_ID"      => "229",
-    "WATER_WORKFLOW_ID" => "16",
-    "DEALING_ASSISTENT_WF_ID" => "6",
-    "WATER_JE_ROLE_ID" => "40",
+    "WATER_MASTER_ID"                   => "3",
+    "TRADE_MASTER_ID"                   => "19",
+    "TRADE_NOTICE_ID"                   => "229",
+    "WATER_WORKFLOW_ID"                 => "16",
+    "DEALING_ASSISTENT_WF_ID"           => "6",
+    "WATER_JE_ROLE_ID"                  => "40",
 
     // User Types
     "USER_TYPES" => [
         "1" => "Citizen",
     ],
+
+    "baseUrl" => "192.168.0.16:8000",
 ];
