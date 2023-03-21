d1d992f151aed55eb74cc5752ffdb4f12df0cf84
SandeepBara <sandeepbara830@gmail.com>
2023-03-21 11:06:06 +0530

    fdg dfg

--- app/Http/Controllers/Property/ReportController.php
+++ app/Http/Controllers/Property/ReportController.php
@@ -339,7 +339,7 @@ class ReportController extends Controller
                 "perPage" => "nullable|digits_between:1,9223372036854775807",
             ]
         );
-        $request->request->add(["metaData" => ["pr9.1", 1.1, null, $request->getMethod(), null,]]);
+        $request->request->add(["metaData" => ["pr10.1", 1.1, null, $request->getMethod(), null,]]);
         return $this->Repository->PropDeactedList($request);  
     }
 }
