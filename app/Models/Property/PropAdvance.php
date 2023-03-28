e4d381a233566b2e9c1b089d1995468f5d934ded
frameworkfuturistic <anshu8581@gmail.com>
2023-03-28 18:47:39 +0530

    Holding TC arrear or advance

--- app/Models/Property/PropAdvance.php
+++ app/Models/Property/PropAdvance.php
@@ -4,6 +4,7 @@ namespace App\Models\Property;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PropAdvance extends Model
 {
@@ -16,5 +17,20 @@ class PropAdvance extends Model
     public function store($req)
     {
         PropAdvance::create($req);
+    }
+
+    /**
+     * | Get Property Advances and Adjust Amount
+     */
+    public function getPropAdvanceAdjustAmt($propId)
+    {
+        return DB::table("prop_advances as a")
+            ->leftJoin("prop_adjustments as p", function ($join) {
+                $join->on("p.prop_id", "=", "a.prop_id");
+            })
+            ->select(DB::raw("sum(coalesce(a.amount, 0)) as advance, sum(coalesce(p.amount, 0)) as adjustment_amt"))
+            ->where("a.prop_id", "=", $propId)
+            ->groupBy("a.amount")
+            ->first();
     }
 }
