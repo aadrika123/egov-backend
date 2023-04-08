d570e9f2c3526072c92176280cb67f21b1a0a491
frameworkfuturistic <anshu8581@gmail.com>
2023-04-08 21:39:26 +0530

    District Wise Collection

--- app/Models/UlbMaster.php
+++ app/Models/UlbMaster.php
@@ -10,4 +10,14 @@ class UlbMaster extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+
+    /**
+     * | Get Ulbs by district code
+     */
+    public function getUlbsByDistrictCode($districtCode)
+    {
+        return UlbMaster::where('district_code', $districtCode)
+            ->get();
+    }
 }
