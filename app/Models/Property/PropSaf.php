25fcea6a032278c1580e0845f54ea099cee9ab1f
mrinal9911 <mrinalk123456@gmail.com>
2023-04-12 11:18:56 +0530

    gbsaf

--- app/Models/Property/PropSaf.php
+++ app/Models/Property/PropSaf.php
@@ -52,6 +52,7 @@ class PropSaf extends Model
                 'doc_upload_status',
                 'payment_status',
                 'role_name as currentRole',
+                'role_name as approvedBy',
                 's.user_id',
                 's.citizen_id',
                 DB::raw(
@@ -63,6 +64,48 @@ class PropSaf extends Model
             ->leftjoin('wf_roles', 'wf_roles.id', 's.current_role')
             ->join('ulb_ward_masters as u', 's.ward_mstr_id', '=', 'u.id')
             ->join('ref_prop_types as p', 'p.id', '=', 's.prop_type_mstr_id')
+            ->leftJoin('ulb_ward_masters as u1', 's.new_ward_mstr_id', '=', 'u1.id')
+            ->first();
+    }
+
+    /**
+     * | Get GB SAf details by saf No
+     */
+    public function getGbSafDtlsBySafNo($safNo)
+    {
+        return DB::table('prop_safs as s')
+            ->where('s.saf_no', strtoupper($safNo))
+            ->select(
+                's.id',
+                's.saf_no',
+                's.ward_mstr_id',
+                's.new_ward_mstr_id',
+                's.prop_address',
+                's.prop_pin_code',
+                's.assessment_type',
+                's.applicant_name',
+                's.application_date',
+                's.area_of_plot as total_area_in_decimal',
+                'u.ward_name as old_ward_no',
+                'u1.ward_name as new_ward_no',
+                'doc_upload_status',
+                'payment_status',
+                // 'role_name as currentRole',
+                'role_name as approvedBy',
+                's.user_id',
+                's.citizen_id',
+                'gb_office_name',
+                'building_type',
+                DB::raw(
+                    "case when s.user_id is not null then 'TC/TL/JSK' when 
+                    s.citizen_id is not null then 'Citizen' end as appliedBy
+                "
+                ),
+            )
+            ->join('wf_roles', 'wf_roles.id', 's.current_role')
+            ->leftjoin('ref_prop_gbpropusagetypes as p', 'p.id', '=', 's.gb_usage_types')
+            ->leftjoin('ref_prop_gbbuildingusagetypes as q', 'q.id', '=', 's.gb_prop_usage_types')
+            ->join('ulb_ward_masters as u', 's.ward_mstr_id', '=', 'u.id')
             ->leftJoin('ulb_ward_masters as u1', 's.new_ward_mstr_id', '=', 'u1.id')
             ->first();
     }
