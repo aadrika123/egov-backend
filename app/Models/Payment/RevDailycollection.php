6d1bd5a01d800e55a3398a7a4f0e6ad48bb79ef3
mrinal9911 <mrinalk123456@gmail.com>
2023-05-09 16:25:24 +0530

    cash verification receipt

--- app/Models/Payment/RevDailycollection.php
+++ app/Models/Payment/RevDailycollection.php
@@ -4,6 +4,7 @@ namespace App\Models\Payment;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class RevDailycollection extends Model
 {
@@ -28,6 +29,7 @@ class RevDailycollection extends Model
     {
         return RevDailycollection::select(
             'rev_dailycollections.id',
+            'ulb_name',
             'u.user_name as tc_name',
             'u.mobile as tc_mobile',
             'uu.user_name as verifier_name',
@@ -35,7 +37,8 @@ class RevDailycollection extends Model
             'tran_no',
             'deposit_amount as amount',
             'module_id',
-            'deposit_date as tran_date',
+            DB::raw("TO_CHAR(demand_date, 'DD-MM-YYYY') as tran_date"),
+            DB::raw("TO_CHAR(deposit_date, 'DD-MM-YYYY') as verification_date"),
             'deposit_mode as payment_mode',
             'cheq_dd_no as cheque_dd_no',
             'bank_name',
@@ -44,6 +47,7 @@ class RevDailycollection extends Model
             ->join('rev_dailycollectiondetails as rdc', 'rdc.collection_id', 'rev_dailycollections.id')
             ->join('users as u', 'u.id', 'rev_dailycollections.tc_id')
             ->join('users as uu', 'uu.id', 'rev_dailycollections.user_id')
+            ->join('ulb_masters', 'ulb_masters.id', 'rev_dailycollections.ulb_id')
             ->where('rev_dailycollections.ulb_id', $ulbId);
     }
 }
