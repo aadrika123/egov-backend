399f757102b6d095a06f8662a670cdf9e6cc363a
frameworkfuturistic <anshu8581@gmail.com>
2023-05-18 12:22:55 +0530

    managing docment managing status in case of btc

--- app/Http/Controllers/Property/SafDocController.php
+++ app/Http/Controllers/Property/SafDocController.php
@@ -418,12 +418,15 @@ class SafDocController extends Controller
                 'action_taken_by' => $userId
             ];
             $mWfDocument->docVerifyReject($wfDocId, $reqs);
-            $ifFullDocVerifiedV1 = $this->ifFullDocVerified($applicationId);
+            if ($req->docStatus == 'Verified')
+                $ifFullDocVerifiedV1 = $this->ifFullDocVerified($applicationId, $req->docStatus);
+            else
+                $ifFullDocVerifiedV1 = 0;                                       // In Case of Rejection the Document Verification Status will always remain false
+
             if ($ifFullDocVerifiedV1 == 1) {                                     // If The Document Fully Verified Update Verify Status
                 $safDtls->doc_verify_status = 1;
                 $safDtls->save();
             }
-
             DB::commit();
             return responseMsgs(true, $req->docStatus . " Successfully", "", "010204", "1.0", responseTime(), "POST", $req->deviceId ?? "");
         } catch (Exception $e) {
@@ -446,11 +449,14 @@ class SafDocController extends Controller
             'moduleId' => FacadesConfig::get('module-constants.PROPERTY_MODULE_ID')
         ];
         $req = new Request($refReq);
-        $refDocList = $mWfActiveDocument->getDocsByActiveId($req);
+        $refDocList = $mWfActiveDocument->getDocsByActiveId($req);      // return Only the Pending Documents 
         // Property List Documents
         $ifPropDocUnverified = $refDocList->contains(function ($item) {
-            return $item->whereIn('verify_status', [0, 2]);                 // 0-Document Pending,1-Document Reject
+            $item = $item->toArray();
+            $a = in_array($item['verify_status'], [0, 2]);                       // 0-Document Pending,1-Document Reject
+            return $a;
         });
+
         if ($ifPropDocUnverified == 1)
             return 0;
         else
