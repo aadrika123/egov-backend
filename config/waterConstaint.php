bffd158a170c2b695e63987f9953cd7d259ddf58
SamKerketta <samkerketta77@gmail.com>
2023-05-24 10:21:18 +0530

    water changes

--- config/waterConstaint.php
+++ config/waterConstaint.php
@@ -25,6 +25,7 @@ return [
     "ACCOUNT_DESCRIPTION" => "Water",
     "DEPARTMENT_SECTION" => "Revenue Section",
     "TOWARDS" => "Water Connection and Others",
+    "TOWARDS_DEMAND" =>"Water User Charges",
     "WATER_DEPAPRTMENT_ID" => "2",
     "WATER_RELATIVE_PATH" => "Uploads/Water",
 
