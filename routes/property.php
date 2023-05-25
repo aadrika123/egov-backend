82f9c5a0aeed89eeff13b535648b8a267ca9dca7
mrinal9911 <mrinalk123456@gmail.com>
2023-05-25 17:11:27 +0530

    sd

--- routes/property.php
+++ routes/property.php
@@ -254,7 +254,6 @@ Route::group(['middleware' => ['json.response', 'auth:sanctum', 'request_logger'
     Route::post('harvesting/btc-inbox', 'btcInboxList');
     Route::post('harvesting/site-verification', 'siteVerification');
     Route::post('harvesting/get-tc-verifications', 'getTcVerifications');
-    // Route::post('harvesting/geotagging', 'geoTagging');
   });
 
   /**
