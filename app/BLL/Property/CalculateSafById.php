9cdaf949f5ece53adab2a6e0ffdc1f197225afe8
frameworkfuturistic <anshu8581@gmail.com>
2023-06-13 18:54:52 +0530

    application approval

--- app/BLL/Property/CalculateSafById.php
+++ app/BLL/Property/CalculateSafById.php
@@ -44,7 +44,6 @@ class CalculateSafById
     public $_firstOwner;
     public $_mPropActiveSafOwners;
     private $_adjustmentAssessmentTypes;
-    public bool $_isTcVerification = false;
 
     public function __construct()
     {
@@ -303,10 +302,8 @@ class CalculateSafById
             else
                 $item['adjust_amount'] = $demand->amount - $demand->balance;
 
-            if ($this->_isTcVerification == false) {
-                if ($item['adjust_amount'] > $item['amount'])                           // If the adjust amount is going high 
-                    $item['adjust_amount'] = $item['amount'];
-            }
+            if ($item['adjust_amount'] > $item['amount'])                           // If the adjust amount is going high 
+                $item['adjust_amount'] = $item['amount'];
 
             $itemAmt = $item['amount'] ?? $item['totalTax'];
             $item['balance'] = roundFigure($itemAmt - $item['adjust_amount']);
