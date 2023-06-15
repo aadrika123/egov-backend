714f2c4d56b650be057acc69fd726a46bf18fac5
SandeepBara <sandeepbara830@gmail.com>
2023-06-15 10:39:16 +0530

    dfgd gd

--- app/Traits/Trade/TradeTrait.php
+++ app/Traits/Trade/TradeTrait.php
@@ -104,6 +104,7 @@ trait TradeTrait
         $owners = collect($ownerDetails)->implode('owner_name', ','); 
         $data = new Collection([
             ['displayString' => 'Ward No', 'key' => 'wardNo', 'value' => $req->ward_no],
+            ['displayString' => 'Holding No', 'key' => 'holdingNo', 'value' => $req->holding_no],
             ['displayString' => 'Owner Name', 'key' => 'ownerName', 'value' => $owners],
             ['displayString' => 'Application Type', 'key' => 'applicationType', 'value' => $req->application_type],
             ['displayString' => 'Ownership Type', 'key' => 'ownershipType', 'value' => $req->ownership_type],
