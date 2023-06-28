1f64cb35011c37889e17e8d8feb833459a5fa05e
frameworkfuturistic <anshu8581@gmail.com>
2023-06-28 13:14:54 +0530

    Saf GB Payment

--- app/BLL/Property/UpdateSafDemand.php
+++ app/BLL/Property/UpdateSafDemand.php
@@ -3,6 +3,7 @@
 namespace App\BLL\Property;
 
 use App\Models\Property\PropSafsDemand;
+use App\Models\Property\PropTranDtl;
 
 /**
  * | For the updation of Saf Generated Demand
@@ -11,16 +12,18 @@ use App\Models\Property\PropSafsDemand;
 class UpdateSafDemand
 {
     private $_mPropSafDemand;
+    private $_mPropTranDtl;
 
     public function __construct()
     {
         $this->_mPropSafDemand = new PropSafsDemand();
+        $this->_mPropTranDtl = new PropTranDtl();
     }
 
     /**
      * | Update Demand
      */
-    public function updateDemand(array $demands): void
+    public function updateDemand(array $demands, $tranId): void
     {
         foreach ($demands as $demand) {
             $propDemand = $this->_mPropSafDemand::findOrFail($demand['id']);
@@ -28,6 +31,15 @@ class UpdateSafDemand
                 'balance' => 0,
                 'paid_status' => 1
             ]);
+
+            // Insert Prop Trans Demand Table
+            $tranReq = [
+                'tran_id' => $tranId,
+                'saf_demand_id' => $demand['id'],
+                'total_demand' => $demand['amount'],
+                'ulb_id' => $demand['ulb_id'],
+            ];
+            $this->_mPropTranDtl->store($tranReq);
         }
     }
 }
