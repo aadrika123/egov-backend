76669e7106553023824656f7686ef83cfbd8da6d
priyaAjoshi <joshi.pj0909@gmail.com>
2023-07-27 11:07:36 +0530

    push

--- routes/notice.php
+++ routes/notice.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Notice\NoticeController;
 
-Route::group(['middleware' => ['json.response', 'auth:sanctum', 'request_logger']], function () {    
+Route::group(['middleware' => ['json.response', "auth_maker"]], function () {
     Route::controller(NoticeController::class)->group(function () {
         Route::post("read-notice-type", "noticeType");
         Route::post("search-application", "serApplication");
