52634e3268112aec4836427adcdcc17dd2dfa815
mrinal9911 <mrinalk123456@gmail.com>
2023-08-02 18:07:26 +0530

    concession

--- app/Http/Controllers/Property/ConcessionController.php
+++ app/Http/Controllers/Property/ConcessionController.php
@@ -811,9 +811,9 @@ class ConcessionController extends Controller
                 ->where('status', 1)
                 ->first();
 
-            // if ($checkExisting)
-            // return responseMsgs(1, "User Already Applied", remove_null($ownerDetails), "", '010711', '01', '303ms-406ms', 'Post', '');
-            return responseMsg(true, "User Already Existing", $ownerDetails);
+            if (isset($checkExisting))
+                return responseMsgs(1, "User Already Applied", remove_null($ownerDetails), "", '010711', '01', '303ms-406ms', 'Post', '');
+            // return responseMsg(true, "User Already Existing", $ownerDetails);
 
             // return responseMsgs(0, "User Not Exists in Workflow", remove_null($ownerDetails), "", '010711', '01', '303ms-406ms', 'Post', '');
             return responseMsg(false, "User Not Exists in Workflow", $ownerDetails);
