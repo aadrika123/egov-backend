cf44b051144877d7e3b95731dd3588dbe5803f95
sandeepBara <sandeepbara830@gmail.com>
2023-08-29 19:11:14 +0530

    jgj

--- app/Models/Workflows/WfActiveDocument.php
+++ app/Models/Workflows/WfActiveDocument.php
@@ -195,7 +195,7 @@ class WfActiveDocument extends Model
     public function getTradeDocByAppNo($applicationId, $workflowId, $moduleId)
     {
         $docUrl = Config::get('module-constants.DOC_URL');
-        return DB::table('wf_active_documents as d')
+        return DB::connection("pgsql_trade")->table('wf_active_documents as d')
             ->select(
                 'd.id',
                 'd.document',
@@ -275,7 +275,7 @@ class WfActiveDocument extends Model
         if (!$workflowId) {
             $workflowId = Config::get('workflow-constants.TRADE_WORKFLOW_ID');
         }
-        $data = DB::table('wf_active_documents as d')
+        $data = DB::connection("pgsql_trade")->table('wf_active_documents as d')
             ->select(
                 'd.id',
                 'd.verify_status',
