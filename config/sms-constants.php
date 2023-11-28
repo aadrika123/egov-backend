6db70af42d1c36386d1275c9dc84edbe93dd7e72
mrinal9911 <mrinalk123456@gmail.com>
2023-11-28 12:07:51 +0530

    sms- helper

---
+++ config/sms-constants.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * | Created at-28-11-2023 
+ * | Created By-Mrinal Kumar
+ * | Created for - SMS Related Constants
+ * | Status-Closed
+ */
+
+return [
+    "jh_user_name"  => env('SMS_USER_NAME'),
+    "jh_password"   => env('SMS_PASSWORD'),
+    "jh_sender_id"  => env('SMS_SENDER_ID'),
+    "jh_secure_key" => env('SMS_SECURE_KEY'),
+    "jh_url"        => env('SMS_URL'),
+];
