b61ffb902cd2beef68887f3aef5b357b194cbe16
pandeyprity <pandeyprity33@gmail.com>
2024-07-29 16:44:25 +0530

    corrected code

--- app/Models/CitizenDesk.php
+++ app/Models/CitizenDesk.php
@@ -39,7 +39,7 @@ class CitizenDesk extends Model
             'citizen_desks.status as is_suspended',
             'citizen_desk_descriptions.id as description_id',
             'citizen_desk_descriptions.heading as desk_description',
-            'citizen_desk_descriptions.status as description_status',
+            'citizen_desk_descriptions.status as is_suspended',
             'citizen_desk_descriptions.links as description_link'
         )
             ->leftjoin('citizen_desk_descriptions', 'citizen_desk_descriptions.desk_id', '=', 'citizen_desks.id')
@@ -60,7 +60,7 @@ class CitizenDesk extends Model
                             'description_id' => $data->description_id,
                             'desk_description' => $data->desk_description,
                             'description_link' => $data->description_link,
-                            'description_status' =>$data->description_status
+                            'is_suspended' =>$data->is_suspended
                         ];
                     })->values() // Ensure nested data has numeric indices
                 ];
--- app/Models/UserManualHeading.php
+++ app/Models/UserManualHeading.php
@@ -59,7 +59,7 @@ class UserManualHeading extends Model
             'user_manual_headings.status as is_suspended',
             'user_manual_heading_descriptions.id as description_id',
             'user_manual_heading_descriptions.description',
-            'user_manual_heading_descriptions.status as description_status',
+            'user_manual_heading_descriptions.status as is_suspended',
             'user_manual_heading_descriptions.user_manual_link',
             'user_manual_heading_descriptions.video_link'
         )
@@ -82,7 +82,7 @@ class UserManualHeading extends Model
                             'description' => $data->description,
                             'user_manual_link' => $data->user_manual_link,
                             'video_link' => $data->video_link,
-                            'description_status' =>$data->description_status
+                            'is_suspended' =>$data->is_suspended
                         ];
                     })->values() // Ensure nested data has numeric indices
                 ];
