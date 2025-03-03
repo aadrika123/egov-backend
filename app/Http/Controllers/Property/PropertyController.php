e1d3d7362e51979a02b76e7bae0608fa4e91bbd3
Arsh620 <arshadhussain82987@gmail.com>
2025-03-03 13:24:51 +0530

    changes

--- app/Http/Controllers/Property/PropertyController.php
+++ app/Http/Controllers/Property/PropertyController.php
@@ -588,8 +588,8 @@ class PropertyController extends Controller
                 $request->merge(["propId" => $propId]);
                 $demand = $holdingTaxController->getHoldingDues($request);
                 $demand = $demand->original;
-                if ($demand['status'] == false) {
-                    throw new Exception("Previous Demand is not clear for the respective property."); // Return false immediately if any demand has status false
+                if ($demand['status'] == true) {
+                    throw new Exception("Previous Demand is not clear for the respective property.". $demand['data']['basicDetails']['holding_no'] ?? 'N/A'); // Return false immediately if any demand has status false
                 }
                 if ($demand['status'] == false) {
                     $demand['data']['basicDetails']['property_id'] = $propId;
