c90f1e3988015ae3a3ca6bf991f9b0e21983ce7d
alokanand524 <alokanand524@gmail.com>
2025-11-10 16:05:34 +0530

    added page in /market route/path to hinde the default laravel page

--- resources/views/market/index.blade.php
+++ resources/views/market/index.blade.php
@@ -1,1 +1,9 @@
-nothing to load .................
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Market</title>
+</head>
+<body>
+    <h1> Laravel Market Page Loaded Successfully</h1>
+</body>
+</html>
