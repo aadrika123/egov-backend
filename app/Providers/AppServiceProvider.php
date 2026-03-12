bf2689a26f1d6105b8a1756d1ad7bd9a15419b97
Aadrika DevOps <devops@aadrika.com>
2026-03-12 15:47:20 +0530

    fix: Jenkinsfile GITOPS_DIR + AppServiceProvider guard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\KitLoong\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+        if (class_exists(\KitLoong\MigrationsGenerator\MigrationsGeneratorServiceProvider::class)) {
+            $this->app->register(\KitLoong\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+        }
     }
 
     /**
